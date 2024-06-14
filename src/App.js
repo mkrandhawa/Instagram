2e560dd84f0905c06819722eecaf2428fcf04bd8
Manpreet Kaur <manpreetkaur@MianSoban.home>
2024-06-14 23:01:19 +0100

    Added routing for birthday component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from './login/loginPage';
 import SignUpPage from "./signUp/signUpPage";
+import BirthdayInfo from "./signUp/birthdayForm";
 
 
 export default function App(){
@@ -11,6 +12,8 @@ export default function App(){
                 <Route exact path='/accounts/login' element={<LoginPage />} />
                 
                 <Route exact path='/accounts/signup' element={<SignUpPage />} />
+
+                <Route exact path='/accounts/signup/birthday' element={<BirthdayInfo />} />
 
                 <Route exact path='/' element={<h1>Hello</h1>} />
 
