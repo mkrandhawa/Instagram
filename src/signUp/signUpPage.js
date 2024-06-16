26a1f8962f2cecf4bf434e1aea3c3dc6ad57467e
mkrandhawa71@gmail.com <manpreetkaur@MianSoban.home>
2024-06-16 15:59:27 +0100

    Removed the phones

--- src/signUp/signUpPage.js
+++ src/signUp/signUpPage.js
@@ -1,14 +1,11 @@
 import React from "react";
 import Footer from "../login/footer";
-// import Phones from '../login/phones';
-
 import SignUpCard from "./signupCard";
 
 export default function SignUpPage (){
     return(
         <>  
         <div className="container signupContainer">
-            {/* <Phones /> */}
             <SignUpCard />
 
         </div>
