94a629651b851e910fe1e8636f0fec0ef8c737e1
mkrandhawa71@gmail.com <manpreetkaur@MianSoban.home>
2024-06-16 16:01:31 +0100

    Removed phone component

--- src/signUp/birthdayPage.js
+++ src/signUp/birthdayPage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Footer from "../login/footer";
-import Phones from '../login/phones';
+// import Phones from '../login/phones';
 import BirthdayCard from "./birthdayCard";
 
 export default function BirthdayPage (){
     return(
         <>  
         <div className="container birthdayContainer">
-            <Phones />
+            {/* <Phones /> */}
             <BirthdayCard />
         </div>
             <Footer />
