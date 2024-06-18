4202054ee4a8181fd1b5bbd4af40bab5eaccc8c0
mkrandhawa71@gmail.com <manpreetkaur@MianSoban.home>
2024-06-18 10:58:43 +0100

    Added input field

--- src/login/inputsFields.js
+++ src/login/inputsFields.js
@@ -3,7 +3,7 @@ import React from "react";
 
 
 export default function Input (props){
-    const{className, type, name, placeholder, maxLength, autoCapitalize, autoCorrect, required, onChange, minLength}= props;
+    const{className, type, name, placeholder, maxLength, autoCapitalize, autoCorrect, required, onChange, minLength, value}= props;
 
     
 
@@ -20,6 +20,7 @@ export default function Input (props){
             autoCorrect={autoCorrect}
             required={required}
             onChange={onChange}
+            value={value}
             />
         </>
     );
