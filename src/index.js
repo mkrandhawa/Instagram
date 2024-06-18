e91feb9c4eb61e6f8ab5c5bb62e22881424a3746
mkrandhawa71@gmail.com <manpreetkaur@MianSoban.home>
2024-06-18 10:59:57 +0100

    Removed browse router and added it to the app component

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import reportWebVitals from './reportWebVitals';
 
 //import LoginPage from "./login/loginPage";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 // //Main Container
 // function App() {
@@ -22,9 +21,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
 reportWebVitals();
