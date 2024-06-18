c5dea0907ba9318270c5654432b1e98f032944cc
mkrandhawa71@gmail.com <mkrandhawa71@gmail.com>
2024-06-18 12:31:40 +0100

    Removed uneccesary comments

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -4,12 +4,6 @@ import React, { createContext, useState } from 'react';
 export const UserContext = createContext();
 
 export default function UserProvider(props) {
-    // const [dob, setDob] = useState({
-    //     month: '',
-    //     day: '',
-    //     year: ''
-    // });
-
    
     const [user, setUser] = useState({
         emailPhone: '',
