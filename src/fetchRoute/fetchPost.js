8dd3c9e074ec77e32196ac60fcb8a0fd83d12d2c
mkrandhawa71@gmail.com <mkrandhawa71@gmail.com>
2024-06-26 19:31:00 +0100

    Chnaged credentials properties to include for cookies

--- src/fetchRoute/fetchPost.js
+++ src/fetchRoute/fetchPost.js
@@ -5,7 +5,7 @@
               method: "POST",
               mode: "cors",
               cache: "no-cache",
-              credentials: "same-origin",
+              credentials: "include",
               headers: {
                 "Content-Type": "application/json",
               },
