cf21947e87b319e2990d4957dcdb32a8eb4f1479
mkrandhawa <mkrandhawa71@gmail.com>
2024-07-06 17:20:56 +0100

    Updated user structure

--- src/home/authorisation.js
+++ src/home/authorisation.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState, useContext} from 'react';
 import SideBar from '../navigation/sideNavigation';
+import { UserContext } from '../context/userContext';
+
 
 export default function CheckAuthorization (){
 
     const[isLogged, setIsLogged] = useState(false);
+    const { setUser } = useContext(UserContext);
 
     useEffect(() => {
         fetch('http://localhost:4000/api/v1/users/home', {
@@ -16,12 +19,14 @@ export default function CheckAuthorization (){
 
             if (data.status === 'success') {
                 setIsLogged(true);
+                setUser(data.data.freshUser);
+
             } else {
                 setIsLogged(false);
             }
         })
         .catch(error => console.error('Error:', error));
-    }, [setIsLogged]);
+    }, [setUser, setIsLogged]);
     
 
     return(
