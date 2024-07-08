c068edf44dbd04d6f8d206c56eb7bfc69f44ed8a
mkrandhawa <mkrandhawa71@gmail.com>
2024-07-08 20:16:18 +0100

    Added the user suggestions to home page, basic

--- src/home/homePage.js
+++ src/home/homePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SideBar from '../navigation/sideNavigation';
 import StoryBar from "../navigation/storyNavigation";
+import UserSuggestions from "../navigation/userSuggestions";
 
 
 
@@ -9,12 +10,15 @@ export default function HomePage(){
         <main className="homeMain">
             <SideBar />
             <div className="homePage">
-                <div>
+                <div className="storyBar"> 
                     <StoryBar />
                 </div>
-                <div>
+                <div className="card">
                     <h1>Hello how are you</h1>
                 </div>  
+                <div className="suggestion">
+                    <UserSuggestions />
+                </div>
             </div>
         </main>
     )
