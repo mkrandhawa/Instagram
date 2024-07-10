7bab7466fc7e35891252a3b0dca685efefb2ec7c
mkrandhawa <mkrandhawa71@gmail.com>
2024-07-10 18:33:18 +0100

    Updated to fetch the user data from the backend

--- src/navigation/userSuggestions.js
+++ src/navigation/userSuggestions.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 export default function UserSuggestions(){
 
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([{}]);
 
     useEffect(()=>{
         fetch('http://localhost:4000/api/v1/users/all-users', {
@@ -11,8 +11,10 @@ export default function UserSuggestions(){
         .then(response=>response.json())
         .then(data => {
             console.log('i am data from usersuggestion', data);
+            console.log(data.users)
             if (data.status === 'success'){
-                setUsers(data.users);
+                setUsers(data.data.users);
+                console.log('this is users', data.data.users);
             }
         })
         .catch(error => console.error('Error:', error));
@@ -22,7 +24,9 @@ export default function UserSuggestions(){
 
     return(
         <>
-            <p>hello </p>
+            {users.map((el, index) => <p key={index}>
+                {el.name}
+            </p>)}
         </>
 
     )
